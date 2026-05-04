61c88bba2f81b90f6ef00c918ee2a1ec44d34f69
Lucia ⋆˚꩜｡ <lucia.clvv@gmail.com>
2026-05-04 12:52:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+print("--- LUCIA'S GAMING DASHBOARD ---")
+
+# Asking for info
+game = input("What game are you playing today? ")
+character = input("Who is your main character? ")
+
+# Displaying it back
+print("\n[STATUS UPDATED]")
+print("Currently Grinding: " + game)
+print("Active Character: " + character)
+print("--------------------------------")
